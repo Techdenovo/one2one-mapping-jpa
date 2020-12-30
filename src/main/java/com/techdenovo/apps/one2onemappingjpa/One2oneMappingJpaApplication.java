bec81a695d15e18fdebd59689de8d58af8920f14
Kalyan Mazumder <kalyaninform@gmail.com>
2020-12-30 12:42:42 +0530

    inital commit

---
+++ src/main/java/com/techdenovo/apps/one2onemappingjpa/One2oneMappingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.techdenovo.apps.one2onemappingjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class One2oneMappingJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(One2oneMappingJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techdenovo/apps/one2onemappingjpa/One2oneMappingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techdenovo.apps.one2onemappingjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class One2oneMappingJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
